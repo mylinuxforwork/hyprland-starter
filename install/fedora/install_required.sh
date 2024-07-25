028a77ba8a9da3d5a50174cc87d7bee8023e2676
Stephan Raabe <mail@stephan-raabe.de>
2024-07-25 14:49:04 +0200

    Updates

--- install/fedora/install_required.sh
+++ install/fedora/install_required.sh
@@ -15,8 +15,4 @@ echo ":: Installing on $distro"
 _installPackages "${installer_packages[@]}";
 
 # Install gum
-if [[ $(_isInstalled "gum") == 1 ]]; then
 install/fedora/gum.sh
-else
-    echo "gum is already installed.";
-fi
