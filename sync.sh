3443a9542ed5e8fded0682292530be89b6172118
Stephan Raabe <mail@stephan-raabe.de>
2025-06-03 20:43:45 +0200

    Updates

--- sync.sh
+++ sync.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rsync -avhp --exclude={'monitor.conf','input.conf'} -I dotfiles/ ~/.mydotfiles/com.ml4w.hyprlandstarter/
+rsync -avhp --exclude={'monitor.conf','input.conf','.gitignore','.git'} -I dotfiles/ ~/.mydotfiles/com.ml4w.hyprlandstarter/
